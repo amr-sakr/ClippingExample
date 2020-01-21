33c7f7bf5ee52a87e7a67e2d85d962ad4785a6e6
amrsakr <amr.sakr@bdaiat.com>
2020-01-21 21:06:53 +0200

    First commit

---
+++ app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -0,0 +1,147 @@
+package com.example.clippingexample
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * The @JvmOverloads annotation instructs the Kotlin compiler to
+ * generate overloads for this function that substitute default parameter values.
+ */
+class ClippedView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private val paint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    //variable of a Path to store locally the path of what has been drawn
+    private val path = Path()
+
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
+    //Set up the coordinates for two columns.
+    private val columnOne = rectInset
+    private val columnTwo = columnOne + rectInset + clipRectRight
+
+    private val rowOne = rectInset
+    private val rowTwo = rowOne + rectInset + clipRectBottom
+    private val rowThree = rowTwo + rectInset + clipRectBottom
+    private val rowFour = rowThree + rectInset + clipRectBottom
+    private val textRow = rowFour + (1.5f * clipRectBottom)
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawBackAndUnclippedRectangle(canvas)
+        drawDifferenceClippingExample(canvas)
+        drawCircularClippingExample(canvas)
+        drawIntersectionClippingExample(canvas)
+        drawCombinedClippingExample(canvas)
+        drawRoundedRectangleClippingExample(canvas)
+        drawOutsideClippingExample(canvas)
+        drawSkewedTextExample(canvas)
+        drawTranslatedTextExample(canvas)
+        // drawQuickRejectExample(canvas)
+    }
+
+    private fun drawClippedRectangle(canvas: Canvas) {
+        /*
+        The Canvas.clipRect(left, top, right, bottom) method reduces the region of the screen that
+         future draw operations can write to. It sets the clipping boundaries to be the spatial
+          intersection of the current clipping rectangle and the rectangle passed into clipRect().
+           There are a lot of variants of the clipRect() method that accept different forms
+           of regions and allow different operations on the clipping rectangle.
+         */
+        canvas.clipRect(clipRectLeft, clipRectTop, clipRectRight, clipRectBottom)
+        // fill the canvas with white color. Only the region inside the clipping rectangle will be filled!
+        canvas.drawColor(Color.WHITE)
+        //Change the color to red and draw a diagonal line inside the clipping rectangle.
+        paint.color = Color.RED
+        canvas.drawLine(
+            clipRectLeft, clipRectTop,
+            clipRectRight, clipRectBottom, paint
+        )
+        //Set the color to green and draw a circle inside the clipping rectangle.
+        paint.color = Color.GREEN
+        canvas.drawCircle(
+            circleRadius, clipRectBottom - circleRadius,
+            circleRadius, paint
+        )
+
+        //Set the color to blue and draw text aligned with the right edge of the clipping rectangle. Use Canvas.drawText() to draw text.
+        paint.color = Color.BLUE
+        // Align the RIGHT side of the text with the origin.
+        paint.textSize = textSize
+        /*
+        Note: The Paint.Align property specifies which side of the text to align to the origin
+         (not which side of the origin the text goes, or where in the region it is aligned!).
+         Aligning the right side of the text to the origin places it on the left of the origin.
+         */
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.drawText(
+            context.getString(R.string.clipping),
+            clipRectRight, textOffset, paint
+        )
+    }
+
+
+    private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
+        canvas.drawColor(Color.GRAY)
+        canvas.save()
+        //Translate to the first row and column position.
+        canvas.translate(columnOne, rowOne)
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+
+    private fun drawDifferenceClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawCircularClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawIntersectionClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawCombinedClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawOutsideClippingExample(canvas: Canvas) {
+    }
+
+    private fun drawTranslatedTextExample(canvas: Canvas) {
+    }
+
+    private fun drawSkewedTextExample(canvas: Canvas) {
+    }
+
+    private fun drawQuickRejectExample(canvas: Canvas) {
+    }
+
+
+}
